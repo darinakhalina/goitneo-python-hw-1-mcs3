cd5b6273b3285697ff2b51ed0695da8ec8c13032
Darina Khalina <darina.khalina@namecheap.com>
2023-10-15 22:10:25 +0200

    chore(hw-1): fix task-1 file

--- hw-1/task-1.py
+++ hw-1/task-1.py
@@ -3,11 +3,13 @@ from datetime import datetime, timedelta
 
 
 def get_birthdays_per_week(users: list[dict]):
+    # create empty list
     birthdays = defaultdict(list)
     today = datetime.now().date()
 
     for user in users:
         name = user["name"]
+        # convert date
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
 
@@ -16,9 +18,13 @@ def get_birthdays_per_week(users: list[dict]):
                 year=today.year + 1
             )
 
+        # if the day of the week is Saturday
         if birthday_this_year.weekday() == 5:
+            # moves birthday to the following Monday
             birthday_this_year += timedelta(days=2)
+        # if the day of the week is Sunday
         elif birthday_this_year.weekday() == 6:
+            # moves birthday to the following Monday
             birthday_this_year += timedelta(days=1)
 
         delta_days = (birthday_this_year - today).days
@@ -28,6 +34,7 @@ def get_birthdays_per_week(users: list[dict]):
     if len(birthdays) == 0:
         print("No birthdays found")
 
+    # sort birthdays
     sorted_birthdays = sorted(birthdays.keys())
 
     for day in sorted_birthdays:
